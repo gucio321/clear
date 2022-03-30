18620f5e63ec4475aa15c66429617cd15dd8dfb8
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-03-30 17:23:54 +0200

    add linter

--- clear.go
+++ clear.go
@@ -1,12 +1,16 @@
+// Package clear contains method used to clear the shell console.
 package clear
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
 )
 
+// ErrUnsupportedPlatform is returned, when cleaning console on
+// current platform is not supported.
 var ErrUnsupportedPlatform = errors.New("unsupported platform")
 
 // Clear clears console.
@@ -21,9 +25,13 @@ func Clear() error {
 		return ErrUnsupportedPlatform
 	}
 
+	// nolint:gosec // it is intendet
 	cmd := exec.Command(commandStr)
 	cmd.Stdout = os.Stdout
-	err := cmd.Run()
 
-	return err
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("error clearing console: %w", err)
+	}
+
+	return nil
 }
